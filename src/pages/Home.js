63bdbffcacc37bf8f051f57477898af274b423d5
rebecca505 <rlaurelfrey@gmail.com>
2025-08-15 04:56:03 -0400

    created file sections

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+// import { Box } from "@chakra-ui/react";
+// import Header from '../components/Header';
+
+export default function Home() {
+  return (
+    <div>
+        {/* <Box bgAttachment="fixed" bgImage="url(...)" /> */}
+        {/* <Box bg="blue"><video src='/media/video-4.mp4' autoPlay loop muted /></Box> */}
+        {/* <Header /> */}
+      <h1>Welcome to My Site</h1>
+      <p>This is the Home page.</p>
+    </div>
+  );
+}
