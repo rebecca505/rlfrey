e16998d14e2fc6b3835b02b82f2c0f62c0f76d24
rebecca505 <rlaurelfrey@gmail.com>
2025-08-15 07:30:01 -0400

    added in spline

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,21 @@
-// import { Box } from "@chakra-ui/react";
-// import Header from '../components/Header';
+import { Box } from "@chakra-ui/react";
+import Spline from '@splinetool/react-spline';
 
 export default function Home() {
   return (
     <div>
-        {/* <Box bgAttachment="fixed" bgImage="url(...)" /> */}
-        {/* <Box bg="blue"><video src='/media/video-4.mp4' autoPlay loop muted /></Box> */}
-        {/* <Header /> */}
+      <Box
+        as="section"
+        position="relative"
+        w="100%"
+        h="200px"
+        overflow="hidden"
+        bg="transparent"
+      >
+        <Box position="absolute" top="0" left="0" w="100%">
+          <Spline scene="https://prod.spline.design/mC26st2WiUIQecBw/scene.splinecode" />
+        </Box>
+      </Box>
       <h1>Welcome to My Site</h1>
       <p>This is the Home page.</p>
     </div>
