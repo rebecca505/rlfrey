350eb44c747f781531e39bbc0924cbb68ed14c2f
rebecca505 <rlaurelfrey@gmail.com>
2025-08-16 07:09:34 -0400

    added in projects:

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,4 +1,4 @@
-import { Grid, AbsoluteCenter, Box, Button, Card, Image, Text, Container, SimpleGrid, CardHeader, CardBody, CardFooter, Flex, HStack, Separator, Avatar, Spinner, Link, SegmentGroup } from "@chakra-ui/react";
+import { AbsoluteCenter, Box, Button, Card, Image, Text, SimpleGrid, CardHeader, CardBody, CardFooter, Flex, HStack, Separator, Link, Tag } from "@chakra-ui/react";
 import Proj from '../components/data/Projects.json'
 import { AiFillEye, AiOutlineCode } from "react-icons/ai";
 
@@ -7,7 +7,7 @@ export default function Projects() {
   return (
     <div>
       <Box position="relative" w="100%" h="100px" ><AbsoluteCenter axis="both"><Text textStyle="2xl">{selectedClass}</Text></AbsoluteCenter></Box>
-      
+
       <SimpleGrid px="20px" gap="20px" minChildWidth="350px">
         {Proj.map(proj =>
           selectedClass === 'All' || proj.class === selectedClass ? (
@@ -21,6 +21,13 @@ export default function Projects() {
                       <Text color="gray.400" fontSize="xs"> by {proj.author} </Text>
                     </Box>
                   </Flex>
+                  {proj.tag !== "" && (
+                    <Box>
+                      <Tag.Root>
+                        <Tag.Label>{proj.tag}</Tag.Label>
+                      </Tag.Root>
+                    </Box>
+                  )}
                 </CardHeader>
                 <CardBody color="gray.500">
                   {proj.description}
@@ -28,7 +35,7 @@ export default function Projects() {
                 <Separator variant="dotted" size="xs" pt="5" />
                 <CardFooter justifyContent="flex-end">
                   <HStack gap="2">
-                    <Button variant="ghost" as={Link} href={proj.view}><AiFillEye /> View</Button>
+                    {/* <Button variant="ghost" as={Link} href={proj.view}><AiFillEye /> View</Button> */}
                     <Button variant="ghost" as={Link} href={proj.code}><AiOutlineCode /> Code</Button>
                   </HStack>
                 </CardFooter>
