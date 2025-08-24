4ff62eccc3c0560137ed0a7d356abce0293757e9
rebecca505 <rlaurelfrey@gmail.com>
2025-08-24 00:19:52 -0400

    added in classes tab

--- src/App.js
+++ src/App.js
@@ -2,26 +2,26 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from "./components/ui/provider"
 import Home from './pages/Home';
 import Projects from './pages/Projects';
-// import Classes from './pages/Classes';
+import Classes from './pages/Classes';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import './App.css';
 function App() {
   return (
     <Provider>
-    <Router>
-      <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-      <Navbar />
-      <div style={{ flex: 1 }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          {/* <Route path="/classes" element={<Classes />} /> */}
-        </Routes></div>
-        <Footer />
+      <Router>
+        <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+          <Navbar />
+          <div style={{ flex: 1 }}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/classes" element={<Classes />} />
+            </Routes></div>
+          <Footer />
         </div>
       </Router>
-      </Provider>
+    </Provider>
   );
 }
 
--- src/pages/Classes.js
+++ src/pages/Classes.js
@@ -1,8 +1,11 @@
+import { AbsoluteCenter, Box, Text } from "@chakra-ui/react";
+
 export default function Classes() {
   return (
     <div>
       <h1>hi</h1>
       <p>This is my classes.</p>
+      <Box position="relative" w="100%" h="100px" ><AbsoluteCenter axis="both"><Text textStyle="2xl">First-Year Fall</Text></AbsoluteCenter></Box>
     </div>
   );
 }
