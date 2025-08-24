5d5b2d7264c616c34974132ebec3e4d6f1dfe51c
rebecca505 <rlaurelfrey@gmail.com>
2025-08-24 02:21:45 -0400

    filted courses by year and sem

--- src/pages/Classes.js
+++ src/pages/Classes.js
@@ -2,19 +2,21 @@ import { AbsoluteCenter, Box, Card, CardBody, CardFooter, CardHeader, SimpleGrid
 import Courses from '../components/data/Classes.json'
 
 export default function Classes() {
+  const selectedYear = 'First';
+  const selectedSem = 'Fall';
+  const c = Courses.filter(course => course.semester === selectedSem & course.year === selectedYear)
   return (
     <div>
       <Box position="relative" w="100%" h="100px" ><AbsoluteCenter axis="both"><Text textStyle="2xl">First-Year Fall</Text></AbsoluteCenter></Box>
 
       <SimpleGrid px="20px" gap="20px" minChildWidth="350px">
-        {Courses.map(course => (
-            <div>
-              <Card.Root key={course.id} h="100%" backgroundImage="url('course.img')">
-                <CardHeader>{course.title}</CardHeader>
-                <CardBody>{course.description}</CardBody>
-                <CardFooter>{course.link}</CardFooter>
-              </Card.Root>
-            </div>))}
+        {c.map(course => (
+          <Card.Root key={course.id} h="100%" backgroundImage={course.img} backgroundPosition="center">
+            <CardHeader>{course.title}</CardHeader>
+            <CardBody>{course.description}</CardBody>
+            <CardFooter>{course.link}</CardFooter>
+          </Card.Root>
+        ))}
       </SimpleGrid>
     </div>
   );
