72b9a8e0d59959213b5a4c7a27507580ef8e8147
rebecca505 <rlaurelfrey@gmail.com>
2025-08-24 02:01:06 -0400

    added in basic structure

--- src/pages/Classes.js
+++ src/pages/Classes.js
@@ -1,26 +1,20 @@
 import { AbsoluteCenter, Box, Card, CardBody, CardFooter, CardHeader, SimpleGrid, Text } from "@chakra-ui/react";
+import Courses from '../components/data/Classes.json'
 
 export default function Classes() {
   return (
     <div>
       <Box position="relative" w="100%" h="100px" ><AbsoluteCenter axis="both"><Text textStyle="2xl">First-Year Fall</Text></AbsoluteCenter></Box>
-      
+
       <SimpleGrid px="20px" gap="20px" minChildWidth="350px">
-      <Card.Root backgroundImage="url('/images/whatscookin.png')">
-        <CardHeader>title</CardHeader>
-        <CardBody>description</CardBody>
-        <CardFooter>link</CardFooter>
-      </Card.Root>
-      <Card.Root backgroundImage="url('/images/whatscookin.png')">
-        <CardHeader>title</CardHeader>
-        <CardBody>description</CardBody>
-        <CardFooter>link</CardFooter>
-      </Card.Root>
-      <Card.Root backgroundImage="url('/images/whatscookin.png')">
-        <CardHeader>title</CardHeader>
-        <CardBody>description</CardBody>
-        <CardFooter>link</CardFooter>
-      </Card.Root>
+        {Courses.map(course => (
+            <div>
+              <Card.Root key={course.id} h="100%" backgroundImage="url('course.img')">
+                <CardHeader>{course.title}</CardHeader>
+                <CardBody>{course.description}</CardBody>
+                <CardFooter>{course.link}</CardFooter>
+              </Card.Root>
+            </div>))}
       </SimpleGrid>
     </div>
   );
