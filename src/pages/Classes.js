38c8ba3bc6d6a5d64859de17792a35c1451f5db8
rebecca505 <rlaurelfrey@gmail.com>
2025-08-24 01:53:06 -0400

    created skeleton structure

--- src/pages/Classes.js
+++ src/pages/Classes.js
@@ -1,11 +1,27 @@
-import { AbsoluteCenter, Box, Text } from "@chakra-ui/react";
+import { AbsoluteCenter, Box, Card, CardBody, CardFooter, CardHeader, SimpleGrid, Text } from "@chakra-ui/react";
 
 export default function Classes() {
   return (
     <div>
-      <h1>hi</h1>
-      <p>This is my classes.</p>
       <Box position="relative" w="100%" h="100px" ><AbsoluteCenter axis="both"><Text textStyle="2xl">First-Year Fall</Text></AbsoluteCenter></Box>
+      
+      <SimpleGrid px="20px" gap="20px" minChildWidth="350px">
+      <Card.Root backgroundImage="url('/images/whatscookin.png')">
+        <CardHeader>title</CardHeader>
+        <CardBody>description</CardBody>
+        <CardFooter>link</CardFooter>
+      </Card.Root>
+      <Card.Root backgroundImage="url('/images/whatscookin.png')">
+        <CardHeader>title</CardHeader>
+        <CardBody>description</CardBody>
+        <CardFooter>link</CardFooter>
+      </Card.Root>
+      <Card.Root backgroundImage="url('/images/whatscookin.png')">
+        <CardHeader>title</CardHeader>
+        <CardBody>description</CardBody>
+        <CardFooter>link</CardFooter>
+      </Card.Root>
+      </SimpleGrid>
     </div>
   );
 }
